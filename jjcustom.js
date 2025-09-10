5303a561833222ebde87669a1e2b18fc5c9d374b
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-09-10 16:25:44 -0400

    Quarto to GU Domains post

--- jjcustom.js
+++ jjcustom.js
@@ -1,5 +1,4 @@
 window.document.addEventListener("DOMContentLoaded", function (event) {
-    var $subtitleDiv = $("<br><span class='subtitle'>Prof. Jeff Jacobs&nbsp;<a href='mailto:jj1088@georgetown.edu' target='_blank'><i class='bi bi-envelope-at ps-1 pe-1'></i></a></span><br><span class='subtitle'>Georgetown University</span>");
-    $('.sidebar-title').append($subtitleDiv);
-  });
-  
+  var $subtitleDiv = $("<br><span class='subtitle'>Jeff Jacobs&nbsp;<a href='mailto:jj1088@georgetown.edu' target='_blank'><i class='bi bi-envelope-at ps-1 pe-1'></i></a></span><br><span class='subtitle'>Data Science and Analytics</span><br><span class='subtitle'>Georgetown University</span>");
+  $('.sidebar-title').append($subtitleDiv);
+});
